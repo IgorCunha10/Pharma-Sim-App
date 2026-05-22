945a615264ddb40d2cac8c91545c3e4a57747523
Igor <gigor497@gmail.com>
2026-05-22 12:52:54 -0300

    created ScannerEvent

---
+++ app/src/main/java/com/stela/pharmasimapp/presentation/viewmodel/ScannerEvent.kt
@@ -0,0 +1,13 @@
+package com.stela.pharmasimapp.presentation.viewmodel
+
+sealed class ScannerEvent {
+
+    data class onOptionSelected(val option: String) : ScannerEvent()
+
+    data object onStartScan : ScannerEvent()
+    data object onStopScan : ScannerEvent()
+
+    data object onShowBottomSheet : ScannerEvent()
+    data object onHideBottomSheet : ScannerEvent()
+    
+}
